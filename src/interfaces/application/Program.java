c76b70f1ddd213a2b128c188c2b9e7bfb9bb1fcc
otaviorodriguess <otaviorodriguesbezerra71@gmail.com>
2026-06-22 22:09:31 -0300

    ajustes exercicio interfaces

--- src/interfaces/application/Program.java
+++ src/interfaces/application/Program.java
@@ -1,3 +1,17 @@
+/*
+Uma locadora brasileira de carros cobra um valor por hora para locações de até12 horas.
+Porém, se a duração da locação ultrapassar 12 horas, a locação será cobrada com base em um valor diário.
+Além do valor da locação, é acrescido nopreço o valor do imposto conforme regras do país que, no caso do Brasil, é 20%para valores até 100.00, ou 15% para valores acima de 100.00.
+Fazer um programa que lê os dados da locação (modelo do carro, instante inicial e final da locação),
+bem como o valor por hora e o valor diário de locação.
+O programa deve então gerar a nota de pagamento (contendo valor da locação, valor do imposto e valor total do pagamento) e informar os dados na tela.
+*/
+
+
+
+
+
+
 package interfaces.application;
 
 import interfaces.model.entities.CarRental;
